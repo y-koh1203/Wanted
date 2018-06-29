e48c6e28792ba9b370c86543040d23719ef3f9de
tnkTaka <taka.abc.hiko@gmail.com>
2018-06-29 19:01:56 +0900

    noroute

--- Makefile
+++ Makefile
@@ -1,5 +1,11 @@
+ENV	:=	development
+
 run:
 	go run server.go
 
 dep:
 	dep ensure
+
+## Migrate db schema
+migrate_up:
+	sql-migrate up -env=$(ENV)
--- server.go
+++ server.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -15,17 +14,17 @@ func main() {
 	router.Static("/assets", "./assets")
 	router.LoadHTMLGlob("./templates/*")
 
-	router.GET("/", func(c *gin.Context) {
+	router.NoRoute(func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", gin.H{})
 	})
 
-	router.POST("/", func(c *gin.Context) {
-		//入力フィールドの値をtextに格納
-		text := c.PostForm("text")
-		fmt.Print(text)
-
-		c.HTML(http.StatusOK, "index.html", gin.H{})
-	})
+	//router.POST("/", func(c *gin.Context) {
+	//	//入力フィールドの値をtextに格納
+	//	text := c.PostForm("text")
+	//	fmt.Print(text)
+	//
+	//	c.HTML(http.StatusOK, "index.html", gin.H{})
+	//})
 
 	router.Run(":5000")
 }
